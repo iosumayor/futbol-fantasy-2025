1df5c25dcef92021348456d0abd6dd5076c06a57
iosumayor <iosumayor23@gmail.com>
2025-09-23 12:13:59 +0200

    feat: vuelve a como estaba el archivo

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-          sdfsdfsdf
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
