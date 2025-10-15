fcba944b01298f8ca6103d557c5b2b7bdcc8cc2b
iosumayor <iosumayor23@gmail.com>
2025-10-15 18:05:09 +0200

    test: para ver si se muestra la imagen

--- src/core/domain/___mocks___/aPlayers.ts
+++ src/core/domain/___mocks___/aPlayers.ts
@@ -8,7 +8,13 @@ export function aPlayer(options: Partial<Player> = {}): Player {
     team: "Equipo Test",
     points: 0,
     price: 0,
+    image:
+      "https://assets.laliga.com/squad/2025/t178/p77318/128x128/p77318_t178_2025_1_002_000.jpg",
+    imageDetail:
+      "https://r2.thesportsdb.com/images/media/player/render/agtip81658424227.png/small",
   };
+
+  ///Se utilizan dos imagens random para evitar que en los tests fallen por no encontrar la imagen
 
   return { ...defaultPlayer, ...options };
 }
