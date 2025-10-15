98231b72cc1eca22c5385ee32ce4a5a708f7c9d0
iosumayor <iosumayor23@gmail.com>
2025-10-15 12:57:05 +0200

    feat: añade pagina de detalle de jugador

---
+++ src/ui/pages/PlayerDetails/PlayerDetails.tsx
@@ -0,0 +1,43 @@
+import { usePlayer } from "@core/services/playersService";
+import React from "react";
+import { useParams } from "react-router-dom";
+import styles from "./PlayerDetails.module.scss";
+import { useNavigate } from "react-router-dom";
+export const PlayerDetails: React.FC = () => {
+  const { id } = useParams();
+  const { data: player, isLoading, isError } = usePlayer(Number(id));
+  const navigate = useNavigate();
+
+  if (isLoading) return <div>Cargando detalles del jugador...</div>;
+  if (isError) return <div>Error al cargar detalles del jugador</div>;
+  return (
+    <>
+      <button
+        className={styles.returnButton}
+        type="button"
+        onClick={() => navigate("/players")}
+        aria-label="Volver al listado de jugadores"
+      >
+        ← Volver al listado de jugadores
+      </button>
+      <div className={styles.detailsContainer}>
+        <div className={styles.left}>
+          {player?.imageDetail && (
+            <img
+              className={styles.playerImage}
+              src={player.imageDetail}
+              alt={player.name}
+            />
+          )}
+          <h1 className={styles.playerName}>{player?.name}</h1>
+        </div>
+        <div className={styles.right}>
+          <p>Posición: {player?.position}</p>
+          <p>Equipo: {player?.team}</p>
+          <p>Puntos: {player?.points}</p>
+          <p>Precio: {player?.price}</p>
+        </div>
+      </div>
+    </>
+  );
+};
