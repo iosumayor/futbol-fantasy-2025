a834d82889472694de5536d7430c08abc0244b6e
iosumayor <iosumayor23@gmail.com>
2025-10-16 11:42:42 +0200

    feat: añade navigate basico para ir al formulario desde Home

---
+++ src/ui/pages/FormularioEntrada/index.ts
@@ -0,0 +1,1 @@
+export { FormularioEntrada } from "./FormularioEntrada";
--- src/ui/pages/Home/Home.tsx
+++ src/ui/pages/Home/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h2>Fantasy Futbol</h2>
       <h2>Home</h2>
+      <button onClick={() => navigate("/formulario-entrada")}>
+        Crear usuario
+      </button>
     </div>
   );
 };
