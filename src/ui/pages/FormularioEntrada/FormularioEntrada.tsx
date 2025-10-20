9a0bf2decc7a498bba933ff4fd8a31b41528bf7a
iosumayor <iosumayor23@gmail.com>
2025-10-20 13:19:14 +0200

    feat: elimina el mensaje de alerta

--- src/ui/pages/FormularioEntrada/FormularioEntrada.tsx
+++ src/ui/pages/FormularioEntrada/FormularioEntrada.tsx
@@ -54,7 +54,6 @@ export const FormularioEntrada: React.FC = () => {
     const step1Data = step1Form.getValues();
     const allData = { ...step1Data, ...data };
     console.log("Datos enviados:", allData);
-    alert("Formulario enviado correctamente");
     navigate("/");
   };
 
