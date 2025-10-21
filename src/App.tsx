1e06bcec478c8cf59a90036889d20fc4ef3f3fbb
iosumayor <iosumayor23@gmail.com>
2025-10-21 12:25:56 +0200

    feat: añade ruta privada en app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PlayerDetails } from "@ui/pages/PlayerDetails/PlayerDetails";
 import { NavBar } from "@ui/components/NavBar";
 import { FormularioEntrada } from "@ui/pages/FormularioEntrada";
+import { Login } from "@ui/pages/Login/Login";
+import { CrearTuLiga } from "@ui/pages/CrearTuLiga/CrearTuLiga";
+import { PrivateRoute } from "@core/auth/PrivateRoute";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +21,15 @@ function App() {
           <Route path="/players" element={<Players />} />
           <Route path="/players/:id" element={<PlayerDetails />} />
           <Route path="/formulario-entrada" element={<FormularioEntrada />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/crear-tu-liga"
+            element={
+              <PrivateRoute>
+                <CrearTuLiga />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
