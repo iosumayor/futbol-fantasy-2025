2c4b8a06d04115647d5dd9eb8dd42a1215d83194
iosumayor <iosumayor23@gmail.com>
2025-10-22 17:47:33 +0200

    feat: añade la prop align para el title

--- src/ui/components/Common/Title/Title.tsx
+++ src/ui/components/Common/Title/Title.tsx
@@ -4,11 +4,26 @@ import styles from "./Title.module.scss";
 interface TitleProps {
   children: React.ReactNode;
   level: 1 | 2 | 3 | 4 | 5 | 6;
+  align?: "left" | "center" | "right";
 }
 
-export const Title: React.FC<TitleProps> = ({ children, level = 1 }) => {
+export const Title: React.FC<TitleProps> = ({
+  children,
+  level = 1,
+  align = "left",
+}) => {
   ///el jsx intrinsic elements permite usar etiquetas HTML dinámicamente
   ///en este caso se usa para crear un título con el nivel especificado
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
-  return <Tag className={`${styles[`title${level}`]}`}>{children}</Tag>;
+  const alignmentClass =
+    align === "left"
+      ? styles.alignLeft
+      : align === "center"
+        ? styles.alignCenter
+        : styles.alignRight;
+  return (
+    <Tag className={`${styles[`title${level}`]} ${alignmentClass}`}>
+      {children}
+    </Tag>
+  );
 };
