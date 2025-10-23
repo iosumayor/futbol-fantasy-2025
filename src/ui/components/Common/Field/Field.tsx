2ed526e7120baea32eebf6163f684d19f1389900
iosumayor <iosumayor23@gmail.com>
2025-10-23 19:24:02 +0200

    feat: lo añade al componente field

--- src/ui/components/Common/Field/Field.tsx
+++ src/ui/components/Common/Field/Field.tsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { ShowPasswordButton } from "@ui/components/ShowPasswordButton/ShowPasswordButton";
 import styles from "./Field.module.scss";
 
 interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
+  isPasswordField?: boolean;
 }
 
 // Usar forwardRef para que React Hook Form pueda pasar ref
 export const Field = React.forwardRef<HTMLInputElement, FieldProps>(
-  ({ label, error, ...props }, ref) => {
+  ({ label, error, isPasswordField, ...props }, ref) => {
     return (
       <div className={styles.field}>
         <label className={styles.label} htmlFor={props.name}>
           {label}
         </label>
         <input className={styles.input} id={props.name} ref={ref} {...props} />
+        {isPasswordField && (
+          <ShowPasswordButton
+            visible={props.type === "text"}
+            onClick={() => {
+              props.type = props.type === "text" ? "password" : "text";
+            }}
+            ariaLabel={
+              props.type === "text" ? "Ocultar contraseña" : "Ver contraseña"
+            }
+          />
+        )}
         {error && <span className={styles.span}>{error}</span>}
       </div>
     );
