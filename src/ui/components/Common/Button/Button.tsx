4ebae35623dff5710c524db8aad8d68cabe668df
iosumayor <iosumayor23@gmail.com>
2025-10-23 11:55:54 +0200

    feat : añade el navBar en el button

--- src/ui/components/Common/Button/Button.tsx
+++ src/ui/components/Common/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Button.module.scss";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: "blue" | "red";
+  variant?: "blue" | "red" | "navBar";
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -11,7 +11,14 @@ export const Button: React.FC<ButtonProps> = ({
   variant = "blue",
   ...props
 }) => {
-  const buttonClass = variant === "blue" ? styles.blueButton : styles.redButton;
+  const variantClassMap = {
+    blue: styles.blueButton,
+    red: styles.redButton,
+    navBar: styles.navButton,
+  };
+
+  const buttonClass = variantClassMap[variant] ?? styles.blueButton; // Default to blueButton if variant not found
+
   return (
     <button type={type} {...props} className={buttonClass}>
       {children}
