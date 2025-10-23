fa5f1ddacbc3a2bc73fcb717d0c1a4f5fbf2fe4e
iosumayor <iosumayor23@gmail.com>
2025-10-23 19:32:40 +0200

    feat: añade el boton de visibilidad al componente field

--- src/ui/components/Common/Field/Field.tsx
+++ src/ui/components/Common/Field/Field.tsx
@@ -6,34 +6,32 @@ interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
   isPasswordField?: boolean;
+  onTogglePassword?: () => void;
 }
-
 // Usar forwardRef para que React Hook Form pueda pasar ref
+
 export const Field = React.forwardRef<HTMLInputElement, FieldProps>(
-  ({ label, error, isPasswordField, ...props }, ref) => {
-    return (
-      <div className={styles.field}>
-        <label className={styles.label} htmlFor={props.name}>
-          {label}
-        </label>
-        <input className={styles.input} id={props.name} ref={ref} {...props} />
-        {isPasswordField && (
-          <ShowPasswordButton
-            visible={props.type === "text"}
-            onClick={() => {
-              props.type = props.type === "text" ? "password" : "text";
-            }}
-            ariaLabel={
-              props.type === "text" ? "Ocultar contraseña" : "Ver contraseña"
-            }
-          />
-        )}
-        {error && <span className={styles.span}>{error}</span>}
-      </div>
-    );
-  },
+  ({ label, error, isPasswordField, onTogglePassword, ...props }, ref) => (
+    <div className={styles.field}>
+      <label className={styles.label} htmlFor={props.name}>
+        {label}
+      </label>
+      <input className={styles.input} id={props.name} ref={ref} {...props} />
+      {isPasswordField && onTogglePassword && (
+        <ShowPasswordButton
+          visible={props.type === "text"}
+          onClick={onTogglePassword}
+          ariaLabel={
+            props.type === "text" ? "Ocultar contraseña" : "Ver contraseña"
+          }
+        />
+      )}
+      {error && <span className={styles.span}>{error}</span>}
+    </div>
+  ),
 );
 
 /// Asignar un displayName para facilitar la depuración
 /// cuando se usa con React.forwardRef y evitar warnings
+
 Field.displayName = "Field";
