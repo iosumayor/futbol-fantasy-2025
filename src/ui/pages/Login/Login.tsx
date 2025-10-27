b4372a3bb0aab9595c20653953221e1831c3e449
iosumayor <iosumayor23@gmail.com>
2025-10-27 12:30:27 +0100

    feat: hook para el useLogin

--- src/ui/pages/Login/Login.tsx
+++ src/ui/pages/Login/Login.tsx
@@ -1,41 +1,11 @@
-import { useAuth } from "@core/auth/useAuth";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.scss";
-import { loginSchema, LoginFormData, MOCK_USER } from "./schemas/login.schema";
 import { Title } from "@ui/components/Common/Title/Title";
 import { Button } from "@ui/components/Common/Button/Button";
 import { Field } from "@ui/components/Common/Field/Field";
+import { useLogin } from "./useLogin";
 
 export const Login: React.FC = () => {
-  const { login } = useAuth();
-  const navigate = useNavigate();
-
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors, isSubmitting },
-  } = useForm<LoginFormData>({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      usuario: "",
-      contraseña: "",
-    },
-  });
-
-  const onSubmit = async (data: LoginFormData) => {
-    if (
-      data.usuario === MOCK_USER.usuario &&
-      data.contraseña === MOCK_USER.contraseña
-    ) {
-      login();
-      navigate("/");
-    } else {
-      setError("contraseña", { message: "Usuario o contraseña incorrectos" });
-    }
-  };
+  const { register, handleSubmit, errors, isSubmitting, onSubmit } = useLogin();
 
   return (
     <div className={styles.loginContainer}>
