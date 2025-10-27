3f9a5c22f4632655f8f57bcd05b98a892066ad67
iosumayor <iosumayor23@gmail.com>
2025-10-27 12:14:31 +0100

    feat: variable para el inputId

--- src/ui/components/Common/Field/Field.tsx
+++ src/ui/components/Common/Field/Field.tsx
@@ -11,29 +11,27 @@ interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 // Usar forwardRef para que React Hook Form pueda pasar ref
 
 export const Field = React.forwardRef<HTMLInputElement, FieldProps>(
-  ({ label, error, isPasswordField, onTogglePassword, ...props }, ref) => (
-    <div className={styles.field}>
-      <label className={styles.label} htmlFor={props.id ?? props.name}>
-        {label}
-      </label>
-      <input
-        className={styles.input}
-        id={props.id ?? props.name}
-        ref={ref}
-        {...props}
-      />
-      {isPasswordField && onTogglePassword && (
-        <ShowPasswordButton
-          visible={props.type === "text"}
-          onClick={onTogglePassword}
-          ariaLabel={
-            props.type === "text" ? "Ocultar contraseña" : "Ver contraseña"
-          }
-        />
-      )}
-      {error && <span className={styles.span}>{error}</span>}
-    </div>
-  ),
+  ({ label, error, isPasswordField, onTogglePassword, ...props }, ref) => {
+    const inputId = props.id ?? props.name;
+    return (
+      <div className={styles.field}>
+        <label className={styles.label} htmlFor={inputId}>
+          {label}
+        </label>
+        <input className={styles.input} ref={ref} {...props} id={inputId} />
+        {isPasswordField && onTogglePassword && (
+          <ShowPasswordButton
+            visible={props.type === "text"}
+            onClick={onTogglePassword}
+            ariaLabel={
+              props.type === "text" ? "Ocultar contraseña" : "Ver contraseña"
+            }
+          />
+        )}
+        {error && <span className={styles.span}>{error}</span>}
+      </div>
+    );
+  },
 );
 
 /// Asignar un displayName para facilitar la depuración
