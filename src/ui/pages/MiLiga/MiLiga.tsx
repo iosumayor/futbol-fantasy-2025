00777ee2e91788b2ccca2439018c4fbff6320098
iosumayor <iosumayor23@gmail.com>
2025-10-29 11:42:16 +0100

    feat: añade pagina inicial de misLigas

--- src/ui/pages/MiLiga/MiLiga.tsx
+++ src/ui/pages/MiLiga/MiLiga.tsx
@@ -1,3 +1,32 @@
+import React from "react";
+import { useAllLigas } from "@core/services/ligasService";
+import { Title } from "@ui/components/Common/Title/Title";
+import { useNavigate } from "react-router-dom";
+
 export const MiLiga: React.FC = () => {
-  return <div>Mi Liga Page</div>;
+  const { data: ligas, isLoading, error } = useAllLigas();
+  const navigate = useNavigate();
+
+  if (isLoading) return <div>Cargando ligas...</div>;
+  if (error) return <div>Error al cargar ligas</div>;
+
+  return (
+    <div className={"container"}>
+      <Title level={1}>Mis Ligas</Title>
+      <ul className="ligasLista">
+        {ligas?.map((liga) => (
+          <li
+            key={liga.id}
+            className="item"
+            onClick={() => navigate(`/liga/${liga.id}`)}
+            tabIndex={0}
+            role="button"
+          >
+            {liga.name}
+            <p>{liga.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
