cc7c20f7075382c10f39b95f590779bd8b6d0d8a
iosumayor <iosumayor23@gmail.com>
2025-11-04 11:20:09 +0100

    feat: añade el titulo de la liga y la descripcion de la misma a la pagina principal de mis ligas

--- src/ui/pages/MiLiga/Components/PaginaGeneralMiLiga/PaginaGeneralMiLiga.tsx
+++ src/ui/pages/MiLiga/Components/PaginaGeneralMiLiga/PaginaGeneralMiLiga.tsx
@@ -1,23 +1,39 @@
 import { useParams, useNavigate } from "react-router-dom";
 import React from "react";
 import styles from "./PaginaGeneralMiLiga.module.scss";
+import { Title } from "@ui/components/Common/Title/Title";
+import { useLigaById } from "@core/services/ligasService";
 
 export const PaginaGeneralMiLiga: React.FC = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const { data, isLoading, isError } = useLigaById(Number(id));
+
+  if (isLoading) return <div>Cargando liga...</div>;
+  if (isError) return <div>Error al cargar la liga</div>;
 
   return (
-    <nav className={styles.ligaNav}>
-      <ul>
-        <li onClick={() => navigate(`/mi-liga/${id}/mercado`)}>Mercado</li>
-        <li onClick={() => navigate(`/mi-liga/${id}/clasificacion`)}>
-          Clasificación
-        </li>
-        <li onClick={() => navigate(`/mi-liga/${id}/plantilla`)}>
-          Mi Plantilla
-        </li>
-        <li onClick={() => navigate(`/mi-liga/${id}/once`)}>Once Titular</li>
-      </ul>
-    </nav>
+    <>
+      <Title align="center" level={1}>
+        {data?.name}
+      </Title>
+      {data?.description && (
+        <Title align="center" level={2}>
+          {data.description}
+        </Title>
+      )}
+      <nav className={styles.ligaNav}>
+        <ul>
+          <li onClick={() => navigate(`/mi-liga/${id}/mercado`)}>Mercado</li>
+          <li onClick={() => navigate(`/mi-liga/${id}/clasificacion`)}>
+            Clasificación
+          </li>
+          <li onClick={() => navigate(`/mi-liga/${id}/plantilla`)}>
+            Mi Plantilla
+          </li>
+          <li onClick={() => navigate(`/mi-liga/${id}/once`)}>Once Titular</li>
+        </ul>
+      </nav>
+    </>
   );
 };
