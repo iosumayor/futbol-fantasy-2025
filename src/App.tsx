fe48cfa12c622ac226b09302d08bf6c253ab12c5
iosumayor <iosumayor23@gmail.com>
2025-11-07 12:26:39 +0100

    feat: aplicamos outler y creamos componente MiLigaLayout

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { PrivateRoute } from "@core/auth/PrivateRoute";
 import { MiLiga } from "@ui/pages/MiLiga/MiLiga";
 import { PaginaGeneralMiLiga } from "@ui/pages/MiLiga/Components/PaginaGeneralMiLiga";
 import { MercadoJugadores } from "@ui/pages/MiLiga/Components/MercadoJugadores";
+import { MiLigaLayout } from "@ui/pages/MiLiga/Components/PaginaGeneralMiLiga/MiLigaLayout";
 
 const queryClient = new QueryClient();
 
@@ -45,18 +46,14 @@ function App() {
             path="/mi-liga/:id"
             element={
               <PrivateRoute>
-                <PaginaGeneralMiLiga />
+                <MiLigaLayout />
               </PrivateRoute>
             }
-          />
-          <Route
-            path="/mi-liga/:id/mercado"
-            element={
-              <PrivateRoute>
-                <MercadoJugadores />
-              </PrivateRoute>
-            }
-          />
+          >
+            <Route index element={<PaginaGeneralMiLiga />} />
+            <Route path="mercado" element={<MercadoJugadores />} />
+            {/* ...otras rutas hijas */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
