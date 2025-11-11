f9d9c739603bc1dff4e3b84dfe1b2faa5b984e61
iosumayor <iosumayor23@gmail.com>
2025-11-11 11:22:18 +0100

    feat: trabaja con datos en el componente de clasificacion

--- src/ui/pages/MiLiga/Components/Clasificacion/Clasificacion.tsx
+++ src/ui/pages/MiLiga/Components/Clasificacion/Clasificacion.tsx
@@ -1,3 +1,38 @@
+import React from "react";
+import { useLigaById } from "@core/services/ligasService";
+import { Title } from "@ui/components/Common/Title/Title";
+import { useParams } from "react-router-dom";
+import styles from "./Clasificacion.module.scss";
+import { useUsuariosByLigaId } from "@core/services/usuariosByLigaService";
+
 export const Clasificacion: React.FC = () => {
-  return <div>Clasificacion Component</div>;
+  const { id } = useParams();
+  const { data: liga, isLoading, isError } = useLigaById(Number(id));
+  const {
+    data: usuarios,
+    isLoading: usuariosLoading,
+    isError: usuariosError,
+  } = useUsuariosByLigaId(Number(id));
+
+  if (isLoading || usuariosLoading) return <div>Cargando clasificación...</div>;
+  if (isError || usuariosError)
+    return <div>Error al cargar la clasificación</div>;
+
+  const usuariosOrdenadosPorPuntos = usuarios?.sort(
+    (a, b) => b.points - a.points,
+  );
+
+  return (
+    <div>
+      <Title level={1}>Clasificacion de la liga de {liga?.name}</Title>
+      <ul className={styles.clasificacionList}>
+        {usuariosOrdenadosPorPuntos?.map((usuarios) => (
+          <li key={usuarios.id} className={styles.clasificacionItem}>
+            <span className={styles.username}>{usuarios.username}</span>
+            <span className={styles.points}>{usuarios.points} puntos</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
