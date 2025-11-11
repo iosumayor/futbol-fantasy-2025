fdf72f5f62f0c5dfe4035db13420809bad1f0800
iosumayor <iosumayor23@gmail.com>
2025-11-11 13:46:08 +0100

    feat: añade infraestructura y servicio de mi plantilla

---
+++ src/core/infrastructure/JsonMiPlantillaRepository.ts
@@ -0,0 +1,29 @@
+import { MiPlantilla } from "@core/domain/MiPlantilla";
+import { MiPlantillaRepository } from "@core/domain/MiPlantillaRepository";
+import miPlantillaData from "../../data/miPlantilla.json";
+
+export class JsonMiPlantillaRepository implements MiPlantillaRepository {
+  async getMiPlantilla(
+    userId: number,
+    ligaId: number,
+  ): Promise<MiPlantilla | null> {
+    const plantilla = miPlantillaData.find(
+      (p) => p.userId === userId && p.ligaId === ligaId,
+    );
+    if (!plantilla) return null;
+
+    // Mapea los jugadores para forzar el tipo de position
+    // Ya que al importar desde JSON pierde el tipo específico
+    return {
+      ...plantilla,
+      jugadores: plantilla.jugadores.map((j) => ({
+        ...j,
+        position: j.position as
+          | "Portero"
+          | "Defensa"
+          | "Centrocampista"
+          | "Delantero",
+      })),
+    };
+  }
+}
---
+++ src/core/services/miPlantillaService.ts
@@ -0,0 +1,11 @@
+import { JsonMiPlantillaRepository } from "@core/infrastructure/JsonMiPlantillaRepository";
+import { useQuery } from "@tanstack/react-query";
+
+const repo = new JsonMiPlantillaRepository();
+
+export function usePlantillaByLigaByPlayer(userId: number, ligaId: number) {
+  return useQuery({
+    queryKey: ["plantilla", userId, ligaId],
+    queryFn: () => repo.getMiPlantilla(userId, ligaId),
+  });
+}
