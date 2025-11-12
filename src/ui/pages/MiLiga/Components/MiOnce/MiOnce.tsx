379e52db8b93df0ae63144900b077761639d864a
iosumayor <iosumayor23@gmail.com>
2025-11-12 18:11:05 +0100

    feat: componente de fondo para mi página

---
+++ src/ui/pages/MiLiga/Components/MiOnce/MiOnce.tsx
@@ -0,0 +1,12 @@
+import { Fondo } from "./components/Fondo";
+// ...formaciones y lógica...
+
+export const MiOnce: React.FC = () => {
+  // ...estado y select...
+
+  return (
+    <div>
+      <Fondo>aasadasda</Fondo>
+    </div>
+  );
+};
