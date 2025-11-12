7cc8a5a033617c2a89b374e1c6ada02ce9d56c1b
iosumayor <iosumayor23@gmail.com>
2025-11-12 11:02:36 +0100

    feat: modifica el nombre de miPlantilla para ser similar al resto

--- src/core/services/miPlantillaService.ts
+++ src/core/services/miPlantillaService.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const repo = new JsonMiPlantillaRepository();
 
-export function usePlantillaByLigaByPlayer(userId: number, ligaId: number) {
+export function useMiPlantilla(userId: number, ligaId: number) {
   return useQuery({
     queryKey: ["plantilla", userId, ligaId],
     queryFn: () => repo.getMiPlantilla(userId, ligaId),
